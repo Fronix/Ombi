03c81faecc5e17317cb177aaaa242f38edd14df2
fronix <fronix@fronix.se>
2019-07-16 16:44:08 +0200

    Remove strict sanitizing for custompage

--- src/Ombi/ClientApp/app/services/custompage.service.ts
+++ src/Ombi/ClientApp/app/services/custompage.service.ts
@@ -13,13 +13,13 @@ import { ServiceHelpers } from "./service.helpers";
 export class CustomPageService extends ServiceHelpers {
     constructor(public http: HttpClient, public platformLocation: PlatformLocation) {
         super(http, "/api/v1/CustomPage", platformLocation);
-    } 
+    }
 
     public getCustomPage(): Observable<ICustomPage> {
-        return this.http.get<ICustomPage>(this.url, {headers: this.headers});
+        return this.http.get<ICustomPage>(this.url, { headers: this.headers });
     }
 
     public saveCustomPage(model: ICustomPage): Observable<boolean> {
-        return this.http.post<boolean>(this.url, model, {headers: this.headers});
+        return this.http.post<boolean>(this.url, model, { headers: this.headers });
     }
 }
